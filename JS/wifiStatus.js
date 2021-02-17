df5ad967b0c213881f706cf8ce7843e85d959c59
Ollie <o.c.booth123@gmail.com>
2021-02-17 16:18:18 +0000

    updated

--- JS/wifiStatus.js
+++ JS/wifiStatus.js
@@ -47,7 +47,7 @@
 const checkOnlineStatus = async () => {
    try {
       
-     const online = await fetch("/1pixel.png");
+     const online = await fetch("1pixel.png");
      return online.status >= 200 && online.status < 300; // either true or false
    } catch (err) {
      return false; // definitely offline
